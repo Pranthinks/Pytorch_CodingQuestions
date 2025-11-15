25884d67ddce28625a2cd1cdd0a57db20cb380a3
Pranthinks <praneeps3999@gmail.com>
2025-11-15 11:00:11 -0600

    Did 5 and 6

--- Practice-2/day2.py
+++ Practice-2/day2.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import math
 
 #x = torch.tensor([1.0, 2.3, 3.9], dtype = torch.float32)
 #y = torch.tensor([9.0, 3.5, 6.7], dtype = torch.float32)
@@ -37,6 +37,7 @@ class Simple_Forward(nn.Module):
 
         return x
 
+'''
 x = torch.rand(3, 4, dtype = torch.float32)
 y = torch.rand(3, 6, dtype = torch.float32)
 
@@ -44,3 +45,54 @@ model = Simple_Forward(4, 5, 6)
 
 op = model(x)
 print(op)
+'''
+
+#Qurstion 5
+
+tokens = torch.tensor([1.2, 3.0, 4.1], dtype = torch.long)
+class Custom_Embed(nn.Module):
+    def __init__(self, embed_size , d_model):
+        super().__init__()
+        self.embed_layer = nn. Embedding(embed_size, d_model)
+    
+    def forward(self, x):
+        x = self.embed_layer(x)
+        return x
+'''
+op = Custom_Embed(5, 2)
+
+print(op(tokens))
+'''
+#Question - 6
+
+class Positional_Encoding(nn.Module):
+    def __init__(self, d_model, max_len):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype = torch.float).unsqueeze(1)
+        first_term = torch.exp((torch.arange(0, d_model, 2).float()) * (-math.log(10000.0)/ d_model))
+
+        pe[: , 0::2] = torch.sin(position * first_term) 
+        pe[:, 1::2] = torch.cos(position * first_term)
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+    
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+    
+
+class Embed_and_pos(nn.Module):
+    def __init__(self, embed_size, d_model, max_len):
+        super().__init__()
+        self.embed = nn.Embedding(embed_size, d_model)
+        self.pos = Positional_Encoding(d_model, max_len)
+    
+    def forward(self, x):
+        x = self.embed(x)
+        x = self.pos(x)
+        return x
+
+x = torch.tensor([[1, 2, 3, 4, 5]])
+
+obj = Embed_and_pos(6, 4, 5)
+print(obj(x))
