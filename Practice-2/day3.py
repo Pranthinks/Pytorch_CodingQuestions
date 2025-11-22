2e2e1a1caeb38f7546898bdddbb957c73c0ed818
Pranthinks <praneeps3999@gmail.com>
2025-11-22 12:16:04 -0600

    Did KV Cache

--- Practice-2/day3.py
+++ Practice-2/day3.py
@@ -34,3 +34,40 @@ def top_p(logits, p: float = 0.8):
     return sorted_indices[mask][sampled_idx]
 
 
+#Question 14 Implementing KV - Cache 
+
+class Custom_KV(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        self.atten = nn.MultiheadAttention(d_model, num_heads)
+    
+
+    def forward(self, x, cache = None):
+        if cache is not None:
+            past_key = cache["key"]
+            past_val = cache["value"]
+        
+        else:
+            past_key = None
+            past_val = None
+        
+        key = torch.cat([past_key, x], dim = 0) if past_key is not None else x
+        val = torch.cat([past_val, x], dim = 0) if past_val is not None else x
+
+        atten_op, atten_wei = self.atten(x, key, val)
+
+        new_val = {
+            "key": key,
+            "value" : val
+        }
+
+        return atten_op, new_val
+
+obj = Custom_KV(4, 2)
+x = torch.ones(2, 1, 4)
+op1, op2 = obj(x, cache = None)
+#print(op2.shape)
+print(op2["key"].shape)
+
+fin1 , fin2 = obj(x, cache = op2)
+print(fin2["key"].shape)
